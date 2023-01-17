vindaxlabs/cometbft

commit 3c2ce9fc9ed43e84c599589220f1e8d99ed51e51

Renaming in docker-related files (#110)
* Performed renaming in docker-related files

* Fix e2e launch

* one more

Makefile
@@ -1,13 +1,13 @@
 PACKAGES=$(shell go list ./...)
 BUILDDIR?=$(CURDIR)/build
-OUTPUT?=$(BUILDDIR)/tendermint
+OUTPUT?=$(BUILDDIR)/cometbft
 
-BUILD_TAGS?=tendermint
+BUILD_TAGS?=cometbft
 
 COMMIT_HASH := $(shell git rev-parse --short HEAD)
 LD_FLAGS = -X github.com/tendermint/tendermint/version.TMGitCommitHash=$(COMMIT_HASH)
 BUILD_FLAGS = -mod=readonly -ldflags "$(LD_FLAGS)"
-HTTPS_GIT := https://github.com/tendermint/tendermint.git
+HTTPS_GIT := https://github.com/cometbft/cometbft.git
 CGO_ENABLED ?= 0
 
 # handle nostrip
@@ -230,7 +230,7 @@ go.sum: go.mod
 draw_deps:
 	@# requires brew install graphviz or apt-get install graphviz
 	go get github.com/RobotsAndPencils/goviz
-	@goviz -i github.com/tendermint/tendermint/cmd/tendermint -d 3 | dot -Tpng -o dependency-graph.png
+	@goviz -i github.com/cometbft/cometbft/cmd/tendermint -d 3 | dot -Tpng -o dependency-graph.png
 .PHONY: draw_deps
 
 get_deps_bin_size:
@@ -246,9 +246,9 @@ get_deps_bin_size:
 
 # generates certificates for TLS testing in remotedb and RPC server
 gen_certs: clean_certs
-	certstrap init --common-name "tendermint.com" --passphrase ""
+	certstrap init --common-name "cometbft.com" --passphrase ""
 	certstrap request-cert --common-name "server" -ip "127.0.0.1" --passphrase ""
-	certstrap sign "server" --CA "tendermint.com" --passphrase ""
+	certstrap sign "server" --CA "cometbft.com" --passphrase ""
 	mv out/server.crt rpc/jsonrpc/server/test.crt
 	mv out/server.key rpc/jsonrpc/server/test.key
 	rm -rf out
@@ -266,7 +266,7 @@ clean_certs:
 
 format:
 	find . -name '*.go' -type f -not -path "*.git*" -not -name '*.pb.go' -not -name '*pb_test.go' | xargs gofmt -w -s
-	find . -name '*.go' -type f -not -path "*.git*"  -not -name '*.pb.go' -not -name '*pb_test.go' | xargs goimports -w -local github.com/tendermint/tendermint
+	find . -name '*.go' -type f -not -path "*.git*"  -not -name '*.pb.go' -not -name '*pb_test.go' | xargs goimports -w -local github.com/cometbft/cometbft
 .PHONY: format
 
 lint:
@@ -284,7 +284,7 @@ DESTINATION = ./index.html.md
 ###                           Documentation                                 ###
 ###############################################################################
 
-DOCS_OUTPUT?=/tmp/tendermint-core-docs
+DOCS_OUTPUT?=/tmp/cometbft-core-docs
 
 # This builds a docs site for each branch/tag in `./docs/versions` and copies
 # each site to a version prefixed path. The last entry inside the `versions`
@@ -329,9 +329,9 @@ check-docs-toc:
 ###############################################################################
 
 build-docker: build-linux
-	cp $(OUTPUT) DOCKER/tendermint
-	docker build --label=tendermint --tag="tendermint/tendermint" DOCKER
-	rm -rf DOCKER/tendermint
+	cp $(OUTPUT) DOCKER/cometbft
+	docker build --label=cometbft --tag="cometbft/cometbft" DOCKER
+	rm -rf DOCKER/cometbft
 .PHONY: build-docker
 
 ###############################################################################
@@ -349,15 +349,15 @@ build-docker-localnode:
 
 # Runs `make build TENDERMINT_BUILD_OPTIONS=cleveldb` from within an Amazon
 # Linux (v2)-based Docker build container in order to build an Amazon
-# Linux-compatible binary. Produces a compatible binary at ./build/tendermint
+# Linux-compatible binary. Produces a compatible binary at ./build/cometbft
 build_c-amazonlinux:
 	$(MAKE) -C ./DOCKER build_amazonlinux_buildimage
-	docker run --rm -it -v `pwd`:/tendermint tendermint/tendermint:build_c-amazonlinux
+	docker run --rm -it -v `pwd`:/cometbft cometbft/cometbft:build_c-amazonlinux
 .PHONY: build_c-amazonlinux
 
 # Run a 4-node testnet locally
 localnet-start: localnet-stop build-docker-localnode
-	@if ! [ -f build/node0/config/genesis.json ]; then docker run --rm -v $(CURDIR)/build:/tendermint:Z tendermint/localnode testnet --config /etc/tendermint/config-template.toml --o . --starting-ip-address 192.167.10.2; fi
+	@if ! [ -f build/node0/config/genesis.json ]; then docker run --rm -v $(CURDIR)/build:/cometbft:Z cometbft/localnode testnet --config /etc/cometbft/config-template.toml --o . --starting-ip-address 192.167.10.2; fi
 	docker-compose up
 .PHONY: localnet-start
 

DOCKER/Makefile
@@ -5,9 +5,9 @@ push:
 	@sh -c "'$(CURDIR)/push.sh'"
 
 build_testing:
-	docker build --tag tendermint/testing -f ./Dockerfile.testing .
+	docker build --tag cometbft/testing -f ./Dockerfile.testing .
 
 build_amazonlinux_buildimage:
-	docker build -t "tendermint/tendermint:build_c-amazonlinux" -f Dockerfile.build_c-amazonlinux .
+	docker build -t "cometbft/cometbft:build_c-amazonlinux" -f Dockerfile.build_c-amazonlinux .
 
 .PHONY: build push build_testing build_amazonlinux_buildimage

networks/local/Makefile
@@ -1,7 +1,7 @@
 # Makefile for the "localnode" docker image.
 
 all:
-	docker build --tag tendermint/localnode localnode
+	docker build --tag cometbft/localnode localnode
 
 .PHONY: all
 

test/e2e/Makefile
@@ -1,7 +1,7 @@
 all: docker generator runner
 
 docker:
-	docker build --tag tendermint/e2e-node --tag tendermint/e2e-node:local-version -f docker/Dockerfile ../..
+	docker build --tag cometbft/e2e-node --tag cometbft/e2e-node:local-version -f docker/Dockerfile ../..
 
 # We need to build support for database backends into the app in
 # order to build a binary with a Tendermint node in it (for built-in

.github/workflows/cometbft-docker.yml
@@ -1,6 +1,6 @@
 name: Docker
-# Build & Push rebuilds the Tendermint docker image on every push to main and creation of tags
-# and pushes the image to https://hub.docker.com/r/cometbft/tendermint
+# Build & Push rebuilds the CometBFT docker image on every push to main and creation of tags
+# and pushes the image to https://hub.docker.com/r/cometbft/cometbft
 on:
   push:
     branches:
@@ -19,7 +19,7 @@ jobs:
       - name: Prepare
         id: prep
         run: |
-          DOCKER_IMAGE=cometbft/tendermint
+          DOCKER_IMAGE=cometbft/cometbft
           VERSION=noop
           if [[ $GITHUB_REF == refs/tags/* ]]; then
             VERSION=${GITHUB_REF#refs/tags/}

DOCKER/.gitignore
@@ -1,1 +1,1 @@
-tendermint
+cometbft

DOCKER/Dockerfile
@@ -2,30 +2,30 @@
 # hopefully current, go version.
 ARG GOLANG_BASE_IMAGE=golang:1.18-alpine
 
-# stage 1 Generate Tendermint Binary
+# stage 1 Generate CometBFT Binary
 FROM --platform=$BUILDPLATFORM $GOLANG_BASE_IMAGE as builder
 RUN apk update && \
     apk upgrade && \
     apk --no-cache add make
-COPY / /tendermint
-WORKDIR /tendermint
+COPY / /cometbft
+WORKDIR /cometbft
 RUN TARGETPLATFORM=$TARGETPLATFORM make build-linux
 
 # stage 2
 FROM $GOLANG_BASE_IMAGE
-LABEL maintainer="hello@tendermint.com"
+LABEL maintainer="hello@informal.systems"
 
-# Tendermint will be looking for the genesis file in /tendermint/config/genesis.json
+# CometBFT will be looking for the genesis file in /cometbft/config/genesis.json
 # (unless you change `genesis_file` in config.toml). You can put your config.toml and
-# private validator file into /tendermint/config.
+# private validator file into /cometbft/config.
 #
-# The /tendermint/data dir is used by tendermint to store state.
-ENV TMHOME /tendermint
+# The /cometbft/data dir is used by CometBFT to store state.
+ENV TMHOME /cometbft
 
 # OS environment setup
 # Set user right away for determinism, create directory for persistence and give our user ownership
 # jq and curl used for extracting `pub_key` from private validator while
-# deploying tendermint with Kubernetes. It is nice to have bash so the users
+# deploying CometBFT with Kubernetes. It is nice to have bash so the users
 # could execute bash commands.
 RUN apk update && \
     apk upgrade && \
@@ -43,11 +43,11 @@ EXPOSE 26656 26657 26660
 
 STOPSIGNAL SIGTERM
 
-COPY --from=builder /tendermint/build/tendermint /usr/bin/tendermint
+COPY --from=builder /cometbft/build/cometbft /usr/bin/cometbft
 
 # You can overwrite these before the first run to influence
 # config.json and genesis.json. Additionally, you can override
-# CMD to add parameters to `tendermint node`.
+# CMD to add parameters to `cometbft node`.
 ENV PROXY_APP=kvstore MONIKER=dockernode CHAIN_ID=dockerchain
 
 COPY ./DOCKER/docker-entrypoint.sh /usr/local/bin/

DOCKER/Dockerfile.build_c-amazonlinux
@@ -21,8 +21,8 @@ ENV PATH=$PATH:/usr/local/go/bin:/go/bin
 ENV GOBIN=/go/bin
 ENV GOPATH=/go/src
 
-RUN mkdir -p /tendermint
-WORKDIR /tendermint
+RUN mkdir -p /cometbft
+WORKDIR /cometbft
 
-CMD ["/usr/bin/make", "build", "TENDERMINT_BUILD_OPTIONS=cleveldb"]
+CMD ["/usr/bin/make", "build", "COMETBFT_BUILD_OPTIONS=cleveldb"]
 

commit 8049b780912c5f8c908ccbb0a4166e912d6d2002

Fix e2e nightlies in CI, broken by #110 (#138)
* Rename TENDERMINT_BUILD_OPTIONS to COMETBFT_BUILD_OPTIONS all over

* Linter error

Makefile
@@ -11,36 +11,36 @@ HTTPS_GIT := https://github.com/cometbft/cometbft.git
 CGO_ENABLED ?= 0
 
 # handle nostrip
-ifeq (,$(findstring nostrip,$(TENDERMINT_BUILD_OPTIONS)))
+ifeq (,$(findstring nostrip,$(COMETBFT_BUILD_OPTIONS)))
   BUILD_FLAGS += -trimpath
   LD_FLAGS += -s -w
 endif
 
 # handle race
-ifeq (race,$(findstring race,$(TENDERMINT_BUILD_OPTIONS)))
+ifeq (race,$(findstring race,$(COMETBFT_BUILD_OPTIONS)))
   CGO_ENABLED=1
   BUILD_FLAGS += -race
 endif
 
 # handle cleveldb
-ifeq (cleveldb,$(findstring cleveldb,$(TENDERMINT_BUILD_OPTIONS)))
+ifeq (cleveldb,$(findstring cleveldb,$(COMETBFT_BUILD_OPTIONS)))
   CGO_ENABLED=1
   BUILD_TAGS += cleveldb
 endif
 
 # handle badgerdb
-ifeq (badgerdb,$(findstring badgerdb,$(TENDERMINT_BUILD_OPTIONS)))
+ifeq (badgerdb,$(findstring badgerdb,$(COMETBFT_BUILD_OPTIONS)))
   BUILD_TAGS += badgerdb
 endif
 
 # handle rocksdb
-ifeq (rocksdb,$(findstring rocksdb,$(TENDERMINT_BUILD_OPTIONS)))
+ifeq (rocksdb,$(findstring rocksdb,$(COMETBFT_BUILD_OPTIONS)))
   CGO_ENABLED=1
   BUILD_TAGS += rocksdb
 endif
 
 # handle boltdb
-ifeq (boltdb,$(findstring boltdb,$(TENDERMINT_BUILD_OPTIONS)))
+ifeq (boltdb,$(findstring boltdb,$(COMETBFT_BUILD_OPTIONS)))
   BUILD_TAGS += boltdb
 endif
 
@@ -347,7 +347,7 @@ build-docker-localnode:
 	@cd networks/local && make
 .PHONY: build-docker-localnode
 
-# Runs `make build TENDERMINT_BUILD_OPTIONS=cleveldb` from within an Amazon
+# Runs `make build COMETBFT_BUILD_OPTIONS=cleveldb` from within an Amazon
 # Linux (v2)-based Docker build container in order to build an Amazon
 # Linux-compatible binary. Produces a compatible binary at ./build/cometbft
 build_c-amazonlinux:

networks/remote/ansible/roles/logzio/tasks/main.yml
@@ -7,10 +7,10 @@
 - name: Create folders
   file: "path={{item}} state=directory recurse=yes"
   with_items:
-  - /etc/journalbeat
-  - /etc/pki/tls/certs
-  - /usr/share/journalbeat
-  - /var/log/journalbeat
+    - /etc/journalbeat
+    - /etc/pki/tls/certs
+    - /usr/share/journalbeat
+    - /var/log/journalbeat
 
 - name: Copy journalbeat config
   template: src=journalbeat.yml.j2 dest=/etc/journalbeat/journalbeat.yml mode=0600
@@ -22,5 +22,5 @@
 - name: Copy journalbeat service config
   copy: src=journalbeat.service dest=/etc/systemd/system/journalbeat.service
   notify:
-  - reload daemon
-  - restart journalbeat
+    - reload daemon
+    - restart journalbeat